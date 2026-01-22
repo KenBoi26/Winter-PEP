c693c521165e793f6cf967ed8b0a971d555e5c85
KenBoi26 <kennethphilip23@lpu.in>
2026-01-22 12:30:45 +0530

    first commit

---
+++ CPP & DSA/CPP/01.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age = 5.5; // does type conversions
+    float num1 = 5.5;
+
+    cout << age << endl;
+    cout << num1 << endl;
+}

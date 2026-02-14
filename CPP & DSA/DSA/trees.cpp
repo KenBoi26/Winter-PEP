3cc9b0149e8f78be4eaae9b06d835f8be51e6278
KenBoi26 <kennethphilip23@lpu.in>
2026-02-14 12:54:38 +0530

    learnt the structure of trees and also the 3 types of traversals

--- CPP & DSA/DSA/trees.cpp
+++ CPP & DSA/DSA/trees.cpp
@@ -28,8 +28,54 @@ class Node{
 
 };
 
-int main(){
+// Inorder traveral
+void inorder(Node* root){
+    if(root == nullptr) return;
 
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+// preorder traversal
+void preorder(Node* root){
+    if(root == nullptr) return;
+
+    cout << root->data << " ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+// postorder traversal
+void postorder(Node* root){
+    if(root == nullptr) return;
+
+    postorder(root->left);
+    postorder(root->right);
+    cout << root->data << " ";
+}
+
+int main(){
+    Node* root = new Node(1);
+
+    root->left = new Node(2);
+    root->right = new Node(3);
+
+    root->left->left = new Node(4);
+    root->left->right = new Node(5);
+    root->right->left = new Node(6);
+    root->right->right = new Node(7);
+
+    cout << "Inorder traversal: ";
+    inorder(root);
+    cout << endl;
+
+    cout << "Preorder traversal: ";
+    preorder(root);
+    cout << endl;
+
+    cout << "Postorder traversal: ";
+    postorder(root);
 
     return 0;
 }

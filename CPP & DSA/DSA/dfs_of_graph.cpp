775b03a29e485f4c74d916effaef05d27ba83c09
KenBoi26 <kennethphilip23@lpu.in>
2026-02-25 03:55:08 +0530

    Implement depth-first search (DFS) algorithm with helper function

--- CPP & DSA/DSA/dfs_of_graph.cpp
+++ CPP & DSA/DSA/dfs_of_graph.cpp
@@ -2,6 +2,28 @@
 using namespace std;
 
 
+void helper(int node, vector<vector<int>> &adj, vector<bool> &visited, vector<int> &result){
+    visited[node] = true;
+    result.push_back(node);
+    
+    for(auto i:adj[node]){
+        if(!visited[i]){
+            helper(i, adj, visited, result);
+        }
+    }
+}
+
+vector<int> dfs(vector<vector<int>>& adj) {
+    // Code here
+    int n = adj.size();
+    
+    vector<bool> visited(n,false);
+    vector<int> result;
+    
+    helper(0, adj, visited, result);
+    return result;
+}
+
 
 int main(){
 
